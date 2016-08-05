c5da58f850be48851f62f15fd26146339ab1cce8
Sublime2Coding <1464125055@qq.com>
2016-08-05 21:42:54 +0800

    添加clean-bat

--- cpFileToDirs.sh
+++ cpFileToDirs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 myvar=3
-while [ $myvar -le 30 ]
+while [ $myvar -le 9 ]
+do
+        dir="0""$myvar""_day/"
+        echo $dir
+        cp make-clean.bat $dir
+        myvar=$(( $myvar + 1 ))
+done
+
+myvar=3
+while [ $myvar -le 9 ]
 do
         dir="$myvar""_day/"
         echo $dir
-        cp run.bat $dir
+        cp make-clean.bat $dir
         myvar=$(( $myvar + 1 ))
 done
